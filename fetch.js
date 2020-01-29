2b4d70ada2f5954f26766e1fc900a8eef67dca38
Manuel Alonge <manuel.alonge@gmail.com>
2020-01-29 12:40:36 +0100

    29012020 12:40 added readme file

--- fetch.js
+++ fetch.js
@@ -1,14 +1,3 @@
-/*
-Step1
-Fare un array di utenti (es. manuelalonge, matteobarone) e fare un ciclo for o while
-Prendere ogni utente e fare una chiamata per ogni utente tramite il ciclo
-Popolare tabella con due righe
-
-Step2
-Generare riga tramite Javascript, così che all'aumentare degli utenti,
-aumenteranno le righe in modo dinamico
-*/
-
 //Step1
 var users = [];
 
---
+++ readme.md
@@ -0,0 +1,11 @@
+Esercizio con Fetch ed Array
+
+Step1
+
+Fare un array di utenti (es. manuelalonge, matteobarone) e fare un ciclo for o while
+Prendere ogni utente e fare una chiamata per ogni utente tramite il ciclo
+Popolare tabella con due righe
+
+Step2
+Generare riga tramite Javascript, così che all'aumentare degli utenti,
+aumenteranno le righe in modo dinamico
