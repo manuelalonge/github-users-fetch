49c1039b3c95e13c60a9029b15d943aab6718b32
Manuel Alonge <manuel.alonge@gmail.com>
2020-01-30 16:19:21 +0100

    30012020 16:19

--- fetch.js
+++ fetch.js
@@ -1,49 +1,47 @@
 //Step1
 var users = [];
 
-for(var i = 0; i < 2; i++) {
+for (var i = 0; i < 2; i++) {
 
-    users.push(prompt('Enter your user name', i));
-
-}
-
-for(var user of users){
-    fetch(`https://api.github.com/users/${user}`).then((response) => {
-
-        return response.json();  
-
-    }).then((responseJson) => {
-
-        injectInfo(responseJson);
-        console.log(responseJson);
-    });
-}
-
-
-function injectInfo(responseJson){
-    
-   /*var table = document.getElementById('myTable');
-    var row = table.insertRow(2);
-    var cells = [];
-   
-    for(var cl = 0; cl < 3; cl++) {
-        cell = row.insertCell(cl);
-        console.log('I did it');                
-    }
-    */
-
-    document.querySelector('.username__cell').innerHTML = responseJson.login;
-    document.querySelector('.avatar__cell').innerHTML = responseJson.avatar_url;
-    document.querySelector('.bio__cell').innerHTML = responseJson.bio;
-
-    var avatar = document.createElement('img');      
-    var src = document.querySelector('.avatar__cell');
-    avatar.src = responseJson.avatar_url;
-    src.appendChild(avatar);
+  users.push(prompt('Enter your Github user name'));
 
 }
 
+for (var user of users) {
+  fetch(`https://api.github.com/users/${user}`).then((response) => {
 
+    return response.json();
 
+  }).then((responseJson) => {
 
+    console.log(responseJson);
+    injectInfo(responseJson);
+
+  });
+}
+
+//Step2
+function injectInfo(responseJson) {
+
+  //How I am trying to populate the other lines
+  var table = document.getElementById('myTable');
+  var row = table.insertRow(2);
+
+  for (var c = 0; c < 3; c++) {
+
+    var cell = row.insertCell(c);
+
+  };
+
+  //How to populate one line
+  document.querySelector('.username__cell').innerHTML = responseJson.login;
+  document.querySelector('.avatar__cell').innerHTML = responseJson.avatar_url;
+  document.querySelector('.bio__cell').innerHTML = responseJson.bio;
+
+  var avatar = document.createElement('img');
+  var src = document.querySelector('.avatar__cell');
+  avatar.src = responseJson.avatar_url;
+  src.appendChild(avatar);
+
+}
 
