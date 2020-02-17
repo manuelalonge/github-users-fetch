89cd68af0a370319cb30092ab93a005c434fe7fa
Manuel Alonge <manuel.alonge@gmail.com>
2020-02-17 16:12:08 +0100

    17022020 16:12

--- Es2/index2.js
+++ Es2/index2.js
@@ -8,16 +8,26 @@ fetch(`https://picsum.photos/v2/list`).then((response) => {
 
 }).then((responseJson) => {
 
-  for(let i = 0; i < responseJson.length; i++){
+  for (let i = 0; i < responseJson.length; i++) {
     console.log(responseJson[i]);
   }
-  
+
   //injectInfo(responseJson);
 
 });
 
 //Step 2 - add photos from picsum api via javascript
 function injectInfo(responseJson) {
+
+  var table = document.getElementById('myTable');
+  var row = table.insertRow(1);
+
+  for (var c = 0; c < 2; c++) {
+
+    var cell = row.insertCell(c);
+    cell.id = `${responseJson.author}_${c}`;
+
+  }
 
 
 
